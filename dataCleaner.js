fc249ace830c68422ca5fb60b1fb85d97e31eb61
Cody Taft <cody.taft@gmail.com>
2018-10-10 14:41:22 -0600

    Create functions to clean data to correct object

---
+++ dataCleaner.js
@@ -0,0 +1,31 @@
+const januaryData = require('./webscraped-data/januaryData.json');
+const februaryData = require('./webscraped-data/februaryData.json');
+const marchData = require('./webscraped-data/marchData.json');
+const aprilData = require('./webscraped-data/aprilData.json');
+const mayData = require('./webscraped-data/mayData.json');
+const juneData = require('./webscraped-data/juneData.json');
+const julyData = require('./webscraped-data/julyData.json');
+const augustData = require('./webscraped-data/augustData.json');
+const septemberData = require('./webscraped-data/septemberData.json');
+const octoberData = require('./webscraped-data/octoberData.json');
+const novemberData = require('./webscraped-data/novemberData.json');
+const decemberData = require('./webscraped-data/decemberData.json');
+
+const cleanData = monthArray => monthArray.map(dayArray => dayArray.map((people) => {
+  const indexOfSpace = people.deadPerson.indexOf(' ');
+  const indexOfComma = people.deadPerson.indexOf(', ');
+  const frontSlicedPerson = people.deadPerson.slice(indexOfSpace + 1);
+  const sliceTheEndOfPerson = frontSlicedPerson.slice(0, indexOfComma);
+  const slicedIndexComma = sliceTheEndOfPerson.indexOf(',');
+  const nameSlicedAgain = sliceTheEndOfPerson.slice(0, slicedIndexComma);
+  const cleanPersonData = {
+    deadPerson: nameSlicedAgain,
+    deathDay: people.deathDay,
+    deathYear: people.deathYear,
+  };
+  return cleanPersonData;
+}));
+
+const cleanMonths = monthData => monthData.map(month => cleanData(month));
+
+console.log(arrayOfMonthData);
