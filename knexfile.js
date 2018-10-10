32ce4c97e0e01dac6f402d1a3b684221ddc8f1b4
Cody Taft <cody.taft@gmail.com>
2018-10-10 17:47:07 -0600

    Add seeds to knex file

--- knexfile.js
+++ knexfile.js
@@ -3,19 +3,22 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/who_are_you',
     migrations: {
-      directory: './db/migrations',
+      directory: './db/migrations'
     },
-    useNullAsDefault: true,
+    seeds: {
+      directory: './seeds'
+    },
+    useNullAsDefault: true
   },
   test: {
     client: 'pg',
     connection: 'postgres://localhost/who_are_you_tests',
     migrations: {
-      directory: './db/migrations',
+      directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/test',
+      directory: './db/seeds/test'
     },
-    useNullAsDefault: true,
-  },
+    useNullAsDefault: true
+  }
 };
