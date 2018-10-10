cb262339c38f1856f78807e48be68111ac36ff15
Cody Taft <cody.taft@gmail.com>
2018-10-10 15:43:09 -0600

    Fixed package.json and started server

--- server.js
+++ server.js
@@ -3,19 +3,20 @@ const express = require('express');
 const app = express();
 
 const environment = process.env.NODE_ENV || 'development';
-const database = require('knex')(configuration);
 const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static('public'));
 
 app.get('/api/v1/deaths', (request, response) => {
-  database('deaths').select()
-    .then((deaths) => {
+  database('deaths')
+    .select()
+    .then(deaths => {
       response.status(200).json(deaths);
     })
-    .catch((error) => {
+    .catch(error => {
       response.status(500).json({ error });
     });
 });
@@ -30,22 +31,23 @@ app.post('/api/v1/deaths', (request, response) => {
     }
   }
 
-  database('deaths').insert(deathData, 'id')
-    .then((death) => {
+  database('deaths')
+    .insert(deathData, 'id')
+    .then(death => {
       response.status(201).json({ id: death[0] });
     })
-    .catch((error) => {
+    .catch(error => {
       response.status(500).json({ error });
     });
 });
 
-
 app.get('/api/v1/dates', (request, response) => {
-  database('dates').select()
-    .then((dates) => {
+  database('dates')
+    .select()
+    .then(dates => {
       response.status(200).json(dates);
     })
-    .catch((error) => {
+    .catch(error => {
       response.status(500).json({ error });
     });
 });
@@ -60,15 +62,15 @@ app.post('/api/v1/dates', (request, response) => {
     }
   }
 
-  database('deaths').insert(dateData, 'id')
-    .then((date) => {
+  database('deaths')
+    .insert(dateData, 'id')
+    .then(date => {
       response.status(201).json({ id: date[0] });
     })
-    .catch((error) => {
+    .catch(error => {
       response.status(500).json({ error });
     });
 });
-
 
 app.listen(app.get('port'), () => {
   console.log(`Who Are You is running on port ${app.get('port')}`);
