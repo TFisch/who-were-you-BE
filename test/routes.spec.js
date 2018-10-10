f5915d17a5616be2dc48649c8a400b8f715e875e
Tim Fischer <emailtimfischer@gmail.com>
2018-10-10 13:29:17 -0600

    Add api routes test

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,4 +1,5 @@
 const chai = require('chai');
+
 const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
@@ -6,7 +7,5 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
-  it('should return the homepage with text', done => {
 
-  });
 })
