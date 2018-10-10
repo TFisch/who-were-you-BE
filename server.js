ddedafb014bc23ac357390d12be2f49bbd010b70
Tim Fischer <emailtimfischer@gmail.com>
2018-10-10 13:32:51 -0600

    Add app listen for server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+  console.log(`Who Are You is running on port ${app.get('port')}`);
+});
