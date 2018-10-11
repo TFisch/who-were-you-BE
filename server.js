bd231fb5fa6b6ce4e0999638bb42f45b9303be8e
Tim Fischer <emailtimfischer@gmail.com>
2018-10-11 14:30:14 -0600

    Add comment delete by id request

--- server.js
+++ server.js
@@ -23,42 +23,6 @@ app.get('/api/v1/deaths', (request, response) => {
     });
 });
 
-app.get('/api/v1/deaths/:id', (request, response) => {
-  database('deaths').where('id', request.params.id).select()
-    .then(deaths => {
-      if (deaths.length) {
-        response.status(200).json(deaths);
-      } else {
-        response.status(404).json({
-          error: `Could not find project with id ${request.params.id}`
-        })
-      }
-    })
-    .catch(error => {
-      response.status(500).json({ error })
-    })
-})
-
-app.post('/api/v1/users', (request, response) => {
-  const userData = request.body;
-  for (const requiredParameter of ['name']['birth_day']['year']) {
-    if (!deathData[requiredParameter]) {
-      return response
-        .status(422)
-        .send({ error: `You're missing a "${requiredParameter}" property.` });
-    }
-  }
-
-  database('users')
-    .insert(userData, 'id')
-    .then(user => {
-      response.status(201).json({ id: user[0] });
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
-});
-
 app.get('/api/v1/users/:id', (request, response) => {
   database('users').where('id', request.params.id).select()
     .then(users => {
@@ -75,6 +39,21 @@ app.get('/api/v1/users/:id', (request, response) => {
     })
 })
 
+app.get('/api/v1/deaths/:id', (request, response) => {
+  database('deaths').where('id', request.params.id).select()
+    .then(deaths => {
+      if (deaths.length) {
+        response.status(200).json(deaths);
+      } else {
+        response.status(404).json({
+          error: `Could not find project with id ${request.params.id}`
+        })
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+})
 
 app.get('/api/v1/dates', (request, response) => {
   database('dates')
@@ -86,6 +65,33 @@ app.get('/api/v1/dates', (request, response) => {
       response.status(500).json({ error });
     });
 });
+
+app.post('/api/v1/users', (request, response) => {
+  const userData = request.body;
+  for (const requiredParameter of ['name']['birth_day']['year']) {
+    if (!deathData[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `You're missing a "${requiredParameter}" property.` });
+    }
+  }
+  database('users')
+    .insert(userData, 'id')
+    .then(user => {
+      response.status(201).json({ id: user[0] });
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.delete('/api/v1/users/:id/comment', (request, response) => {
+  database('users').where({ id: request.params.id }).select('comment')
+
+    .then(response => {
+
+    })
+})
 
 app.delete('/api/v1/users/:id', (request, response) => {
   database('users').where({ id: request.params.id }).del()
