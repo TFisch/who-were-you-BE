05bf70c3a6d36aeaaca98fbf86db987bf4bb2a9d
Tim Fischer <emailtimfischer@gmail.com>
2018-10-10 20:17:30 -0600

    Test to see if all dead people are returned from /deaths GET

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -7,7 +7,17 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 describe('API Routes', () => {
-
+  describe('GET /api/v1/deaths', () => {
+    it('should return all of the dead people', done => {
+      chai.request(server)
+        .get('/api/v1/deaths')
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          done();
+        });
+    });
+  });
 
 })
 
