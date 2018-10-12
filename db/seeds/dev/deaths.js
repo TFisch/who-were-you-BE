31e8a486d562af50fbd70494e350e51aaa259501
Cody Taft <cody.taft@gmail.com>
2018-10-12 08:34:40 -0600

    Attempt to seed data

--- db/seeds/dev/deaths.js
+++ db/seeds/dev/deaths.js
@@ -1,6 +1,12 @@
-const deaths = require('../../allData');
-let deathsData = deaths;
-let daysOfYear = [];
+const deaths = require('../../../allData');
+let deathData = deaths;
+let datesData = [];
+
+deathData.forEach(death => {
+  if (!datesData.find(date => date.day === death.deathDay)) {
+    datesData.push({ day: death.deathDay, astrology_sign: death.astroSign });
+  }
+});
 
 const userData = [
   {
@@ -16,51 +22,48 @@ const userData = [
   }
 ];
 
-const createDates = (knex, death) => {
+const createDate = (knex, date) => {
   return knex('dates')
     .insert(
       {
-        day: death.deathDay,
-        astrology_sign: death.astroSign
+        day: date.day,
+        astrology_sign: date.astrology_sign
       },
       'id'
     )
     .then(dateId => {
       let deathPromises = [];
 
-      deathPromises.push(
-        createDeath(knex, {
-          person_name: death.deadPerson,
-          day_id: dateId[0],
-          year: death.deathYear
-        })
-      );
+      deathData.forEach(death => {
+        deathPromises.push(
+          createDeath(knex, {
+            person_name: death.deadPerson,
+            day_id: dateId[0],
+            year: death.deathYear
+          })
+        );
+      });
+      return Promise.all(deathPromises);
     });
-  return Promise.all(deathPromises);
 };
 
 const createDeath = (knex, death) => {
   return knex('deaths').insert(death);
 };
 
-exports.seed = function(knex, Promise) {
+exports.seed = (knex, Promise) => {
   return knex('users')
     .del()
     .then(() => knex('deaths').del())
     .then(() => knex('dates').del())
     .then(() => {
-      userData.forEach(user => {
-        knex('users').insert(user);
-      });
-    })
-    .then(() => {
       let datePromises = [];
 
-      deathsData.forEach(death => {
-        datePromises.push(createDates(knex, death));
+      datesData.forEach(date => {
+        datePromises.push(createDate(knex, date));
       });
-
       return Promise.all(datePromises);
     })
+    .then(() => console.log('Seeding complete!'))
     .catch(error => console.log(`Error seeding data: ${error}`));
 };
